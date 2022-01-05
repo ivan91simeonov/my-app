40a656a2d37f6c562c7cea1ab7ea8d96c9fc7450
ivan91simeonov <vanches91@abv.bg>
2022-01-05 11:35:29 +0200

    my-app

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,15 @@
 function Register() {
     return (
-        <div>
-          <form action="">
+        <div className="register">
+          <form className="formRegister">
               <label htmlFor="">Email</label>
-              <input type="text" name="email"  placeholder="Enter email"/>
+              <input type="email" name="email"  placeholder="Enter email"/>
+
+              <label htmlFor="">Password</label>
+              <input type="password" name="password"  placeholder="Enter password"/>
 
               <label htmlFor="">Email</label>
-              <input type="text" name="password"  placeholder="Enter password"/>
-
-              <label htmlFor="">Email</label>
-              <input type="text" name="rePas"  placeholder="Repeat password"/>
+              <input type="rePas" name="rePas"  placeholder="Repeat password"/>
 
               <input type="submit" value="Register" />
           </form>
