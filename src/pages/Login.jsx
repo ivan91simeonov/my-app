2489d5274bda3969c29cddabc61f3e3f282828ce
ivan91simeonov <vanches91@abv.bg>
2022-01-05 11:35:41 +0200

    my-app

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        <form>
-            <label htmlFor="">Email</label>
-            <input type="text" name="email" placeholder="email" />
+    // const [showPassword , setShowPassword] = useState(false);
+    // const [formData , setFormData] = useState({
+    //     email: '',
+    //     password
+    // })
+    // const {email , password} = formData ;
+    // const navigate = useNavigate();
 
-            <label htmlFor="">Password</label>
-            <input type="text" name="password" placeholder="password" />
+  return (
+    <div className="login">
+      <header>
+          <p>Welcome Back</p>
+      </header>
+      <form className='formLogin'>
+        <label htmlFor="">Email</label>
+        <input type="email" name="email" placeholder="email" />
 
+        <label htmlFor="">Password</label>
+        <input type="password" name="password" placeholder="password" />
 
-           <input type="submit" value="Submit" />
-        </form>
-        </div>
-    )
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
